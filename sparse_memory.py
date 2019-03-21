6b401a8970cd570474557a14ca7ba0914324549c
Xirid <peter.k.albert@gmail.com>
2019-03-21 22:58:24 +0100

    Update sparse_memory.py

--- sparse_memory.py
+++ sparse_memory.py
@@ -40,8 +40,8 @@ class SparseMemory(nn.Module):
     # if self.print_tensors: print(f"mem_size: {self.mem_size}")
     self.read_heads = read_heads
     self.num_lists = num_lists if num_lists is not None else int(self.mem_size / 100)
-    #self.index_checks = max(self.num_lists // 20, self.num_lists) if index_checks is None else index_checks
-    self.index_checks =index_checks
+    self.index_checks = max(self.num_lists // 20, self.num_lists) if index_checks is None else index_checks
+    #self.index_checks =index_checks
     self.direct_write = direct_write
     #n needs to be exchanged to true token lenght
     self.s = 2
@@ -350,6 +350,7 @@ class SparseMemory(nn.Module):
     # we search for k cells per read head
     if self.print_tensors: print("sparse read now")
     for batch in range(b):
+
       distances, positions = indexes[batch].search(keys[batch])
       
       if self.print_tensors: print("bathc positions")
@@ -398,7 +399,7 @@ class SparseMemory(nn.Module):
     read_positions = torch.unique(read_positions, sorted=False, dim=1)
     self.vis_size = read_positions.size(1)
     
-    # expand to get all the w dimension locations
+    #expand to get all the w dimension locations
     print("read positions and memory size")
     print(read_positions)
     print(memory.size())

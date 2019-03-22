9fc17612f68c6902c58759dfd3f25c93e1f97c4d
Xirid <peter.k.albert@gmail.com>
2019-03-22 17:33:20 +0100

    Update sparse_memory.py

--- sparse_memory.py
+++ sparse_memory.py
@@ -268,7 +268,7 @@ class SparseMemory(nn.Module):
     I = T.ge(I,  1).float()
     erase_matrix = I.unsqueeze(2).expand(self.b, self.vis_size, self.cell_size)
     if self.print_tensors: print(f"write vector {write_vector}")
-    #write_weights[0].fill_(55)
+    write_weights[0].fill_(55)
     writings = T.matmul(write_weights.unsqueeze(3), write_vector)
     if self.print_tensors: print(f"writings before sum {writings}")
     writings = T.sum(writings, dim=1)
@@ -355,7 +355,7 @@ class SparseMemory(nn.Module):
     read_positions = []
     keys = keys.view(b, s* self.read_heads, -1)
     # we search for k cells per read head
-    keys[0].fill_(55)
+    #keys[0].fill_(55)
     if self.print_tensors: print("sparse read now")
     if self.print_tensors: print("positions")
     for batch in range(b):

fixed softmax and added read strength and 500k as base value

lambada15.py
@@ -34,6 +34,8 @@ experiment = Experiment(api_key="zMVSRiUzF89hdX5u7uWrSW5og",
                         project_name="general", workspace="xirider")
 
 
+
+
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s -   %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
                     level=logging.INFO)
@@ -504,6 +506,9 @@ def main():
     parser.add_argument("--read_gate",
                 action='store_true',
                 help="whether to use read gate")
+    parser.add_argument("--tensorboard",
+                action='store_true',
+                help="whether to track weights and memories in tensorboard")
 
     args = parser.parse_args()
 
@@ -511,6 +516,11 @@ def main():
     hyperparams = args.__dict__
 
     experiment.log_parameters(hyperparams)
+
+    if args.tensorboard:
+        from tensorboardX import SummaryWriter
+        writer = SummaryWriter()
+
 
     if args.local_rank == -1 or args.no_cuda:
         device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
@@ -738,6 +748,9 @@ def main():
             train_sampler = DistributedSampler(train_dataset)
         train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
 
+        if args.tensorboard:
+            model.ut.encoder.layer.inter_results = args.inter_results
+            model.ut.encoder.layer.tensorboard = writer
 
 
         
@@ -752,7 +765,10 @@ def main():
 
                 if step == lendata or step == lendata-1:
                     break
-                
+
+                if args.tensorboard:
+                    model.ut.encoder.layer.outer_steps = step
+
                 batch = tuple(t.to(device) for t in batch)
                 input_ids, input_mask, segment_ids, lm_label_ids  = batch
                 loss, predictions = model(input_ids, segment_ids, input_mask, lm_label_ids)
@@ -807,7 +823,11 @@ def main():
                     joinedrealwords = " ".join(realwords)
                     print(joinedrealwords)
                     # print("memory of model:")
-                    # print(model.ut.encoder.layer.memory_hidden["memory"][0].abs().sum(1))
+                    if args.tensorboard:
+                        for name, param in model.ut.named_parameters():
+                            writer.add_histogram(name, param.clone().cpu().data.numpy(), step)
+                        
+                        # writer.add_histogram(model.ut.encoder.layer.memory_hidden["memory"][0].abs().sum(1))
 
 
 
@@ -815,20 +835,20 @@ def main():
                 nb_tr_examples += input_ids.size(0)
                 nb_tr_steps += 1
                 if (step + 1) % args.gradient_accumulation_steps == 0:
-                    if args.fp16:
-                        # modify learning rate with special warm up BERT uses
-                        # if args.fp16 is False, BertAdam is used that handles this automatically
-                        lr_this_step = args.learning_rate * warmup_linear(global_step/num_train_optimization_steps, args.warmup_proportion)
-                        hyperparams["learning_rate"] = lr_this_step
-                        for param_group in optimizer.param_groups:
-                            param_group['lr'] = lr_this_step
+                    # if args.fp16:
+                    #     # modify learning rate with special warm up BERT uses
+                    #     # if args.fp16 is False, BertAdam is used that handles this automatically
+                    #     lr_this_step = args.learning_rate * warmup_linear(global_step/num_train_optimization_steps, args.warmup_proportion)
+                    #     hyperparams["learning_rate"] = lr_this_step
+                    #     for param_group in optimizer.param_groups:
+                    #         param_group['lr'] = lr_this_step
                     optimizer.step()
                     optimizer.zero_grad()
                     experiment.log_metric("current_lr", optimizer.current_lr , step = step)
                     global_step += 1
                 
 
-
+        writer.close()
         # Save a trained model
         logger.info("** ** * Saving fine - tuned model ** ** * ")
 

modeling.py
@@ -33,6 +33,7 @@ from torch import nn
 from torch.nn import CrossEntropyLoss
 
 from file_utils import cached_path
+import torchvision.utils as vutils
 
 from transformer_memory import SparseMemory
 
@@ -416,7 +417,7 @@ class BertAttentionDNC(nn.Module):
         temp_out = self.dropout(temp_out)
         temp_out = temp_out.expand_as(input_tensor)
 
-        read_embedding_output = self.dropout(self.read_embedding(reads_pos))
+        read_embedding_output = self.dropout2(self.read_embedding(reads_pos))
 
         self_output = self.self(input_tensor + temp_out + read_embedding_output , attention_mask)
         attention_output = self.output(self_output, input_tensor)
@@ -506,6 +507,13 @@ class BertLayerDNC(nn.Module):
         self.memory_hidden = None
         self.sum_type = config.sum_type
 
+        self.tensorboard = False
+        self.outer_steps = 0
+        self.inter_results = 0
+        self.mem_save = False
+        self.usage_save = False
+        self.num_hidden_layers = config.num_hidden_layers
+
         self.norm_before = False
 
         self.LayerNorm = BertLayerNorm(config.hidden_size, eps=1e-12)
@@ -516,7 +524,7 @@ class BertLayerDNC(nn.Module):
         if reset_memory:
             self.memory_hidden = self.memory.reset(batch_size, token_number, self.memory_hidden, erase= erase_memory)
         # use sam to read and write tokens, input mask to show dnc which positions should not read and write
-        read_tokens, self.memory_hidden = self.memory(hidden_states, self.memory_hidden, attention_mask = attention_mask)
+        read_tokens, self.memory_hidden, interpolation_gate, write_gate, read_gate = self.memory(hidden_states, self.memory_hidden, attention_mask = attention_mask)
 
         # concat tokens
         hidden_states = torch.cat([hidden_states, read_tokens], dim=1)
@@ -571,6 +579,40 @@ class BertLayerDNC(nn.Module):
         # also gather from attention mask with  i_topk
         gathered_mask = torch.gather(attention_mask, dim=1, index=index_tensor)
         # put
+
+        if self.tensorboard:
+            if self.outer_steps % self.inter_results == 0:
+                for elementname, element in self.memory_hidden.items():
+                    if elementname == "indexes":
+                        pass
+
+                    elif elementname == "memory":
+                        pass
+                        # mem_img = element.clone().abs().sum(2).view(element.size(0),1,1,element.size(1))
+
+                        # #grid = vutils.make_grid(mem_img, normalize=True, scale_each=False)
+                        # #self.tensorboard.add_image(str(self.outer_steps)+"memory"+str(ut_time), grid, ut_time)
+                        # if self.mem_save is False:
+                        #     self.mem_save = mem_img
+                        # else:
+                        #     self.mem_save = torch.cat((self.mem_save, mem_img),2)
+                        # if ut_time == self.num_hidden_layers -1:
+                        #     grid = vutils.make_grid(self.mem_save, normalize=True, scale_each=False)
+                        #     self.tensorboard.add_image(str(self.outer_steps)+"memory", grid, self.outer_steps)
+                            
+                        #     self.mem_save = False
+                    elif elementname == "usage":
+                        us_img = element.clone()
+                        usgrid = vutils.make_grid(us_img, normalize=True, scale_each=False)
+                        self.tensorboard.add_image(str(self.outer_steps)+"usage"+str(ut_time), usgrid, ut_time)
+                    else:
+                        self.tensorboard.add_histogram(str(self.outer_steps)+elementname, element.clone().cpu().data.numpy(), ut_time)
+
+                self.tensorboard.add_histogram(str(self.outer_steps)+"read_tokens", read_tokens[0].clone().abs().sum(1).cpu().data.numpy(), ut_time)
+                self.tensorboard.add_histogram(str(self.outer_steps)+"interpolation_gate", interpolation_gate[0].clone().cpu().data.numpy(), ut_time)
+                self.tensorboard.add_histogram(str(self.outer_steps)+"write_gate", write_gate[0].clone().cpu().data.numpy(), ut_time)
+                self.tensorboard.add_histogram(str(self.outer_steps)+"read_gate", read_gate[0].clone().cpu().data.numpy(), ut_time)
+
 
 
         return layer_output_p, gathered_mask
@@ -667,7 +709,7 @@ class BertEncoderDNC(nn.Module):
         all_encoder_layers = []
 
         for ut_time in range(self.hidden_layer_number):
-            erase_memory = False
+            erase_memory = True
             if ut_time == 0:
                 reset_memory = True
             else:

transformer_memory.py
@@ -5,10 +5,25 @@ from torch.autograd import Variable as var
 import torch.nn.functional as F
 import numpy as np
 import math
+from torch import nn
 
 from util import *
 import time
 
+class BertLayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        """Construct a layernorm module in the TF style (epsilon inside the square root).
+        """
+        super(BertLayerNorm, self).__init__()
+        self.weight = nn.Parameter(T.ones(hidden_size))
+        self.bias = nn.Parameter(T.zeros(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x - u).pow(2).mean(-1, keepdim=True)
+        x = (x - u) / T.sqrt(s + self.variance_epsilon)
+        return self.weight * x + self.bias
 
 class SparseMemory(nn.Module):
 
@@ -25,7 +40,8 @@ class SparseMemory(nn.Module):
       gpu_id=-1,
       mem_gpu_id=-1,
       direct_write=False,
-      read_gate=False
+      read_gate=False,
+      read_strength = True
       #x added direct write, independent false
   ):
     super(SparseMemory, self).__init__()
@@ -57,39 +73,45 @@ class SparseMemory(nn.Module):
     m = self.mem_size
     w = self.cell_size
     r = self.read_heads
+    self.read_strength = read_strength
+    self.spiky = 500000
     # The visible memory size: (K * R read heads, and least used memory cell)
     self.c = (self.K * r) + 1
 
-    if self.independent_linears:
-      if self.gpu_id != -1:
-        self.read_query_transform = nn.Linear(self.input_size, w * r).cuda()
-        self.write_vector_transform = nn.Linear(self.input_size, w).cuda()
-        self.interpolation_gate_transform = nn.Linear(self.input_size, self.c).cuda()
-        self.write_gate_transform = nn.Linear(self.input_size, 1).cuda()
-      else:
-        self.read_query_transform = nn.Linear(self.input_size, w * r)
-        self.write_vector_transform = nn.Linear(self.input_size, w)
-        self.interpolation_gate_transform = nn.Linear(self.input_size, self.c)
-        self.write_gate_transform = nn.Linear(self.input_size, 1)
-      T.nn.init.orthogonal(self.read_query_transform.weight)
-      T.nn.init.orthogonal(self.write_vector_transform.weight)
-      T.nn.init.orthogonal(self.interpolation_gate_transform.weight)
-      T.nn.init.orthogonal(self.write_gate_transform.weight)
-    else:
+    # if self.independent_linears:
+    #   if self.gpu_id != -1:
+    #     self.read_query_transform = nn.Linear(self.input_size, w * r).cuda()
+    #     self.write_vector_transform = nn.Linear(self.input_size, w).cuda()
+    #     self.interpolation_gate_transform = nn.Linear(self.input_size, self.c).cuda()
+    #     self.write_gate_transform = nn.Linear(self.input_size, 1).cuda()
+    #   else:
+    #     self.read_query_transform = nn.Linear(self.input_size, w * r)
+    #     self.write_vector_transform = nn.Linear(self.input_size, w)
+    #     self.interpolation_gate_transform = nn.Linear(self.input_size, self.c)
+    #     self.write_gate_transform = nn.Linear(self.input_size, 1)
+    #   T.nn.init.orthogonal(self.read_query_transform.weight)
+    #   T.nn.init.orthogonal(self.write_vector_transform.weight)
+    #   T.nn.init.orthogonal(self.interpolation_gate_transform.weight)
+    #   T.nn.init.orthogonal(self.write_gate_transform.weight)
+    # else:
       #x depending on directly writing or not, create enough outputs
-      if self.direct_write:
-        #x read query w, sparse read plus lru gates, write gate
-        self.interface_size = (w * r) + 1+ 1
-      else:
-        #x with addition write vector calculation
-        self.interface_size = (w * r) + w + 1 + 1
-      if self.read_gate:
-        self.interface_size += 1
-      if self.gpu_id != -1:
-        self.interface_weights = nn.Linear(self.input_size, self.interface_size).cuda()
-      else:
-        self.interface_weights = nn.Linear(self.input_size, self.interface_size)
-      T.nn.init.orthogonal_(self.interface_weights.weight)
+    if self.direct_write:
+      #x read query w, sparse read plus lru gates, write gate
+      self.interface_size = (w * r) + 1+ 1
+    else:
+      #x with addition write vector calculation
+      self.interface_size = (w * r) + w + 1 + 1
+    if self.read_gate:
+      self.interface_size += 1
+    if self.read_strength:
+      self.interface_size +=1
+    if self.gpu_id != -1:
+      self.interface_weights = nn.Linear(self.input_size, self.interface_size).cuda()
+      self.layernorm = BertLayerNorm(self.interface_size - 1 if self.read_strength else self.interface_size).cuda()
+    else:
+      self.interface_weights = nn.Linear(self.input_size, self.interface_size)
+      self.layernorm = BertLayerNorm(self.interface_size)
+    T.nn.init.orthogonal_(self.interface_weights.weight)
 
     # creates and 5x5 identitiy
     self.I = cuda(1 - T.eye(self.c).unsqueeze(0), gpu_id=self.gpu_id)  # (1 * n * n)
@@ -158,7 +180,6 @@ class SparseMemory(nn.Module):
           
           "visible_memory": cuda(T.zeros(b, c, w).fill_(δ), gpu_id=self.mem_gpu_id),
 
-          "write_weights": cuda(T.zeros(b, s).fill_(δ), gpu_id=self.gpu_id).contiguous(),
           "read_vectors": cuda(T.zeros(b, r, w).fill_(δ), gpu_id=self.gpu_id).contiguous(),
           #n need to add one place for each readhead instead of just 1
           "least_used_mem": cuda(T.arange(0, s).expand(b, s), gpu_id=self.gpu_id).long().contiguous(),
@@ -173,7 +194,6 @@ class SparseMemory(nn.Module):
     elif not erase:
       hidden["memory"] = hidden["memory"].clone().detach()
       hidden["visible_memory"] = hidden["visible_memory"].clone().detach()
-      hidden["write_weights"] = hidden["write_weights"].clone().contiguous().detach()
       hidden["read_vectors"] = hidden["read_vectors"].clone().contiguous().detach()
       hidden["least_used_mem"] = hidden["least_used_mem"].clone().contiguous().detach()
       hidden["usage"] = hidden["usage"].clone().contiguous().detach() / (self.timestep +1)
@@ -190,7 +210,6 @@ class SparseMemory(nn.Module):
           
           "visible_memory": cuda(T.zeros(b, c, w).fill_(δ), gpu_id=self.mem_gpu_id),
 
-          "write_weights": cuda(T.zeros(b, s).fill_(δ), gpu_id=self.gpu_id).contiguous(),
           "read_vectors": cuda(T.zeros(b, r, w).fill_(δ), gpu_id=self.gpu_id).contiguous(),
           #n need to add one place for each readhead instead of just 1
           "least_used_mem": cuda(T.arange(0, s).expand(b, s), gpu_id=self.gpu_id).long().contiguous(),
@@ -302,7 +321,10 @@ class SparseMemory(nn.Module):
 
     new_mask = attention_mask.unsqueeze(2).expand(self.b, self.s, self.vis_size).float()
 
-    write_weights = write_weights * new_mask
+    no_write = hidden["read_positions"] != 0
+    no_write = no_write.unsqueeze(1).expand(self.b, self.s, self.vis_size).float()
+    
+    write_weights = write_weights * new_mask * no_write
 
     if self.print_tensors: print(f"write_weight: {write_weights}")
     # store the write weights
@@ -316,15 +338,14 @@ class SparseMemory(nn.Module):
     if self.print_tensors: print(f"write vector {write_vector}")
     #write_weights[0].fill_(55)
     # returns something illogical, maybe expand and just do a standard multiplyn
+    #n need to check
     writings = T.matmul(write_weights.unsqueeze(3), write_vector)
     if self.print_tensors: print(f"writings before sum {writings}")
     writings = T.sum(writings, dim=1)
+
     if self.print_tensors: print(f"writings after sum {writings}")
     # write into memory
     hidden["visible_memory"] = (hidden["visible_memory"] * (1 - erase_matrix)) + writings
-
-    abc = hidden["visible_memory"]
-    if self.print_tensors: print(f"visible memory {abc}")
     
     
     hidden = self.write_into_sparse_memory(hidden)
@@ -391,7 +412,6 @@ class SparseMemory(nn.Module):
     #read_positions = T.tensor([[ 0,  3,  4,  9, 10], [0,0,0,0,0],[0,0,0,0,0 ],[ 0,0,0,0,0]])
     #usage = usage.clone().contiguous()
     # bug in pytorch when not calling contigous on scattered tensor
-
     usage.scatter_(dim = 1, index= read_positions, src= relevant_usages)
 
 
@@ -423,7 +443,6 @@ class SparseMemory(nn.Module):
 
     read_positions = T.stack(read_positions, 0)
 
-    
 
     if self.print_tensors: print("read positions")
     if self.print_tensors: print(read_positions.size())
@@ -457,8 +476,17 @@ class SparseMemory(nn.Module):
     if self.print_tensors: print(read_positions)
     read_positions = read_positions.view(b, -1)
     # deduplicate all read positions
+    read_chunks = T.chunk(read_positions, b, dim=0)
+    unique_chunks = []
+    #chunk_max = 0
+    for batches in range(b):
+      chunk = T.unique(read_chunks[batches], sorted=False)
+      unique_chunks.append(chunk)
+      # chunk_max = max(chunk_max,len(chunk))
+    # read_positions = T.stack(unique_chunks, 0)
+    read_positions = nn.utils.rnn.pad_sequence(unique_chunks,batch_first=True)
 
-    read_positions = torch.unique(read_positions, sorted=False, dim=1)
+    # read_positions = torch.unique(read_positions, sorted=False, dim=1)
     self.vis_size = read_positions.size(1)
     
     #expand to get all the w dimension locations
@@ -470,7 +498,19 @@ class SparseMemory(nn.Module):
     # take the vectors of the sparse reads and lru and let the read heads each look for the most similiar vector, then do softmax among all the vectors
     # for each head (b x r x (r*k + lru))
     # output shape (b x r x m), where m = r * K + 1
-    read_weights = σ(θ(visible_memory, keys), 2)
+    
+
+    # ke = keys.abs().sum(2)
+    # vis = visible_memory.abs().sum(2)
+    #cosinedistance2 =  deepmindcosine(keys, visible_memory)
+    #import pdb; pdb.set_trace()
+    cosinedistance = θ(visible_memory, keys) * self.spiky * self.saved_read_strength
+    print(self.saved_read_strength[0].sum(0))
+    # read_weights = σ(cosinedistance, 2)
+    # import pdb; pdb.set_trace()
+    #cosinedistance = 
+    read_weights =nn.functional.softmax(cosinedistance, dim=2)
+    # import pdb; pdb.set_trace()
     # let each head return one vector based on the previous softmax (b x r x w)
     # mask out the padding tokens
     new_mask = attention_mask.unsqueeze(2).expand(b, self.s, self.vis_size).float()
@@ -535,6 +575,13 @@ class SparseMemory(nn.Module):
     #   write_gate = F.sigmoid(self.write_gate_transform(ξ).view(b, 1))
     # else:
     ξ = self.interface_weights(e)
+
+    if self.read_strength and self.read_gate:
+      self.saved_read_strength = T.sigmoid(ξ[:, :, -3].contiguous()).view(b, s, 1) 
+      ξ = T.cat([ξ[:,:, :-3], ξ[:,:,-2:]],2 )
+
+    ξ = self.layernorm(ξ)
+
     # r read keys (b * r * w)
     read_query = ξ[:, :, :r * w].contiguous().view(b, s, r, w)
     # write key (b * 1 * w)
@@ -554,6 +601,9 @@ class SparseMemory(nn.Module):
       write_gate = T.sigmoid(ξ[:, :, -1].contiguous()).view(b, s, 1)
     if self.read_gate:
       read_gate = T.sigmoid(ξ[:, :, -2].contiguous()).view(b, s, 1)
+
+    else:
+      read_gate = None
     self.timestep += 1
     #x changed order to first read then write
     read_vectors, hidden = self.read(read_query, hidden, attention_mask)
@@ -562,6 +612,6 @@ class SparseMemory(nn.Module):
     if self.read_gate:
       read_vectors = read_vectors * read_gate.expand(b,s,w)
 
-    return read_vectors, hidden
+    return read_vectors, hidden, interpolation_gate, write_gate, read_gate
     # hidden = self.write(interpolation_gate, write_vector, write_gate, hidden)
     # return self.read(read_query, hidden)